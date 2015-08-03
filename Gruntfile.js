a9857873fae503cc078b68a5a9ddcfda4ca9c8d1
brian chavez <bchavez.gd@gmail.com>
2015-08-03 14:15:08 -0700

    not sure how its working but it is.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,39 @@
 module.exports = function (grunt) {
 	'use strict';
 
+	// vars for directories.
+	var SRC = '/src/works',
+		DEST = '_site/',
+		SASSDIR = '/src/works/sass',
+		CSSdir = DEST + '/css',
+		CONTENT = SRC + '/content';
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('./package.json'),
+
+
 
 		// assemble
 		assemble: {
 			options: {
-				layout: 'page.hbs',
-				layoutdir: './src/works/layouts/',
-				partials: './src/works/partials/**/*.hbs'
+				// Needs {{> body }} in layouts when using layoutdir
+				layoutdir: SRC + '/layouts/',
+				// partials need extention explictly defined.
+				partials: SRC + '/partials/**/*.hbs',
+				// data file
+				data: SRC + '/data/*.{json, yml}'
 			},
 			posts: {
 				files: [
 					{
 						cwd: './src/content/', // nav to dir
-						dest: './_site/', // process to this dir w/ folder structure intact
+						dest: DEST, // process to this dir w/ folder structure intact
 						expand: true,
-						src: ['**/*.hbs','**/*.md', '!_pages/**/*.hbs'] // files to process
+						src: ['**/*.hbs', '**/*.md', '!_pages/**/*.hbs'] // files to process
 },
 					{
 						cwd: './src/content/_pages/', // nav to dir
-						dest: './_site/', // proccess to this dir w/folders intact
+						dest: DEST, // proccess to this dir w/folders intact
 						expand: true,
 						src: '**/*.hbs' // folders/files to process
 }]
@@ -29,9 +41,9 @@ module.exports = function (grunt) {
 		}
 		// end assemble
 	});
-	
+
 	grunt.loadNpmTasks('assemble');
-	
+
 	grunt.registerTask('default', ['assemble']);
-	
+
 };
