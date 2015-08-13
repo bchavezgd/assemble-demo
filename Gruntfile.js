d95ffde0052f0a6c58eba70e40d60b15d7b5ee26
brian chavez <bchavez.gd@gmail.com>
2015-08-13 12:44:44 -0700

    got correct file structure finally!

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,17 +8,27 @@ module.exports = function (grunt) {
 		// Assembles your email content with html layout
 		assemble: {
 			options: {
+				// calls a default layout, needs layoutdir: or full path.
 				layout: 'page.hbs',
+				// needed when using layout.
 				layoutdir: './src/works/layouts',
+				// where to look for partials - no default.
 				partials: './src/works/partials/**/*.hbs',
+				// data dir. no default
 				data: './src/works/data/*.{json,yml}',
-				helpers: './src/works/helpers',
+				// helpers: './src/works/helpers',
+				// flatten creates a weird file structure in the dest dir
 				flatten: false
 			},
+			// pages is a built in assemble collection.
 			pages: {
-				src: ['./src/content/_pages/**/*.hbs'],
-				dest: './_site',
-				layout: 'page.hbs'
+				files: [{
+					expand: true,
+					flatten: false,
+					cwd: 'src/content/_pages/',
+					src: ['**/*.hbs'],
+					dest: '_site'
+				}]
 			}
 		},
 
